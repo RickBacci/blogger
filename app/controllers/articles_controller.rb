130e9fe7661601438a449524b8b868e8e62ecd12
Rick Bacci <rickbacci@gmail.com>
2015-06-03 14:27:32 -0600

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  def new
+    
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
+  private
+
+  def article_params
+    
+  end
+end
